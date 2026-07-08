940306fcfcae602a3df30a208dc2edc9f5f07c5f
Benjamin Arratia Santibañez <benjamin.arratia.santibanez@gmail.com>
2026-07-08 14:35:20 -0400

    feat: omnia v0.1.0 formula with release checksums

--- Formula/omnia.rb
+++ Formula/omnia.rb
@@ -14,7 +14,7 @@ class Omnia < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/Velion-SpA/omnia/releases/download/v0.1.0/omnia_0.1.0_darwin_amd64.tar.gz"
-      sha256 "TODO_REPLACE_WITH_REAL_SHA256_DARWIN_AMD64"
+      sha256 "2eb07af84b8aa3c9d0483957b3c6f91ab589e982b0cbfa437b4534be6c32985a"
 
       def install
         bin.install "omnia"
@@ -22,7 +22,7 @@ class Omnia < Formula
     end
     if Hardware::CPU.arm?
       url "https://github.com/Velion-SpA/omnia/releases/download/v0.1.0/omnia_0.1.0_darwin_arm64.tar.gz"
-      sha256 "TODO_REPLACE_WITH_REAL_SHA256_DARWIN_ARM64"
+      sha256 "e9321656a345227dd0dce507ee17e3ae4b28d30ef89295404f53bf7b52a8308c"
 
       def install
         bin.install "omnia"
@@ -33,7 +33,7 @@ class Omnia < Formula
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://github.com/Velion-SpA/omnia/releases/download/v0.1.0/omnia_0.1.0_linux_amd64.tar.gz"
-      sha256 "TODO_REPLACE_WITH_REAL_SHA256_LINUX_AMD64"
+      sha256 "8233e81a234a2a3041266d75b9a69c2bd1502e4feaefd89ee27fa383c1c291e9"
 
       def install
         bin.install "omnia"
@@ -41,7 +41,7 @@ class Omnia < Formula
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/Velion-SpA/omnia/releases/download/v0.1.0/omnia_0.1.0_linux_arm64.tar.gz"
-      sha256 "TODO_REPLACE_WITH_REAL_SHA256_LINUX_ARM64"
+      sha256 "5608e837e9f9c4a8c10acb0204557129bef989d3a5ddd61b6c406ae1cac81327"
 
       def install
         bin.install "omnia"
